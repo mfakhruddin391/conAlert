bee8afdd2d25192359ae117aede93f58fd34b3f3
mfakhruddin391 <38089994+mfakhruddin391@users.noreply.github.com>
2023-01-01 14:24:41 +0800

    Push and commit all conAlert file to new repo

---
+++ README.md
@@ -0,0 +1,2 @@
+# ConAlert
+The purpose of this apps is to avoid malaysian citizen from investing to a potentially fraud Company.# conAlert
---
+++ android/app/src/main/kotlin/com/example/izitscam/MainActivity.kt
@@ -0,0 +1,6 @@
+package gregartechindustries.izitscam
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
